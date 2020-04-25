754c889f347d9b6bfaf6f753e2bd7c19831e2d08
recurn <61036489+recurn@users.noreply.github.com>
2020-04-24 18:06:59 -0700

    Finished commenting processing file

--- public/js/processing.js
+++ public/js/processing.js
@@ -1,5 +1,5 @@
 
-
+//this is where the user's Heads Up Display (HUD) is rendered in the sidebar
 
 let level_hud;
 let streak_hud;
@@ -7,8 +7,11 @@ let xpBonus_hud;
 let nextLvlXp_hud;
 let xp_hud;
 
+
+//run once at start of session
 function setup() {
 
+    //creating the canvas where this sketch will be rendered
     let canvas = createCanvas(300, 300);
     canvas.parent('container');
     textAlign(CENTER, CENTER);
@@ -26,19 +29,23 @@ function renderHUD() {
     background(255);
     clear();
 
-
+    //white opaque background where the xp  circle will be rendered
     fill(255);
     strokeWeight(0);
     ellipse(width / 2, 55, 100, 100);
 
+    //transparent circle with slight yellow tinge. Makes background for xp
     fill(255, 255, 0, 10);
     strokeWeight(3)
     ellipse(width / 2, 55, 100, 100);
 
+    //shows the current xp as a yellow circle. Size is the ratio between current xp and max xp. The circle will grow with xp gained
     strokeWeight(2);
     stroke(0, 3)
     fill(255, 255, 0, 175);
     ellipse(width / 2, 55, xp_hud / nextLvlXp_hud * 100 - 2);
+    
+    
     let xOffset = 10;
     let yOffset = 0;
 
@@ -46,6 +53,8 @@ function renderHUD() {
     fill(255);
     strokeWeight(3)
 
+    //the streak and bonus xp spaces
+    //filled blue
     fill(52, 168, 235);
     beginShape();
     vertex(width / 2 - 60 - xOffset, 45 + yOffset);
@@ -66,7 +75,8 @@ function renderHUD() {
     endShape(CLOSE);
 
 
-    //ellipse(width/2, 55 + 50, 50, 50);
+    //Level space
+    //filled green
     fill(15, 214, 125);
     beginShape();
     vertex(width / 2 - 20, 85);
@@ -76,6 +86,7 @@ function renderHUD() {
     vertex(width / 2 - 20, 115);
     endShape(CLOSE);
 
+    //boxes for all the titles
     fill(255);
     rectMode(CENTER);
     rect(width / 2, 85, 45, 13);
@@ -83,13 +94,15 @@ function renderHUD() {
     rect(width / 2 - 55, 38, 35, 13);
     rect(width / 2 + 55, 38, 35, 13);
 
-
+    //only display the values if they have been loaded!
     if (!(streak_hud == undefined) && !(xpBonus_hud === undefined)) {
         strokeWeight(0.6);
         fill(0);
         textSize(12);
         text(streak_hud, width / 2 - 55, 54);
         textSize(12);
+
+        //have to adjust %bonus xp text size or it may not fit in space
         if (streak_hud >= 10) {
             textSize(11)
         }
@@ -98,6 +111,8 @@ function renderHUD() {
         }
         text(xpBonus_hud, width / 2 + 55, 54);
     }
+
+    //put all the titles for each space in their appropriate boxes
     fill(0);
     strokeWeight(0.6);
     textSize(10);
@@ -106,12 +121,15 @@ function renderHUD() {
     text("Streak", width / 2 - 55, 38);
     text("Bonus", width / 2 + 55, 38);
 
+
+    //display the current and max xp levels 
     if (nextLvlXp_hud && xp_hud) {
         fill(0);
         textSize(15);
         text(`${xp_hud}/${nextLvlXp_hud}`, width / 2, 55);
     }
 
+    //display the levels
     textSize(20);
     strokeWeight(1);
     if (!(level_hud === undefined)) {
@@ -119,14 +137,9 @@ function renderHUD() {
     }
 }
 
+//run once at the start of the program. 
 function draw() {
-    scale(1.3);
-    translate(-width / 4 + 5, 0);
-    renderHUD();
+    scale(1.3); //adjusting the size 
+    translate(-width / 4 + 5, 0); //move to the top left corner
+    renderHUD();//actually draw the thing
 }
-
-
-// button.addEventListener('click', (e) => {
-//     xp += 10;
-//     render();
-// })
