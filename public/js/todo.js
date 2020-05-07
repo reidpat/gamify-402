f4b2a627d74c28c0dcd97b8e869b6ef6037ce342
recurn <61036489+recurn@users.noreply.github.com>
2020-05-07 15:40:52 -0700

    moved vue todo component to its own file

---
+++ public/js/todo.js
@@ -0,0 +1,48 @@
+let vueTodo = new Vue({
+    el: '#todo',
+    //start with an empty list of tasks
+    data: {
+      newTaskName: '',
+      userID: "",
+      tasks: [],
+    },
+    methods: {
+      //this will update our
+      toggleComplete: function (task) {
+        task.complete = !task.complete;
+        compTask = firebase.functions().httpsCallable('completeTask');
+        compTask(task);
+      },
+      //gets triggered when the user enters a new task
+      handleFormSubmit: async function () {
+        //if we add a new task and we have less than 3 in our list, send that new one to our database
+        if (this.tasks.length < 3) {
+          addTask = firebase.functions().httpsCallable('addTask');
+          addTask({ newTaskName: this.newTaskName });
+        }
+      },
+      handleDelete: function (task) {
+        removeTask = firebase.functions().httpsCallable('removeTask');
+        removeTask(task);
+      },
+      setHoverTrue: function (task) {
+        task.hover = true;
+      },
+      setHoverFalse: function (task) {
+        task.hover = false;
+      },
+      addUser: function (user) {
+        this.userID = user.uid;
+        this.updateTasks();
+      },
+      updateTasks: function (snapshot) {
+        let tasks = [];
+        snapshot.forEach(doc => {
+          tasks.push({ ...doc.data(), id: doc.id, hover: false })
+        });
+        this.tasks = tasks;
+      },
+    },
+    mounted() {
+    }
+  });
